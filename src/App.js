2105db1f42ec1ed9f5f0c4d3f6a9694ed24bd7a3
AjneoDalipi <ajneodalipi04@gmail.com>
2023-01-26 22:24:15 +0100

    Working with data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './index.css';
-import { useState } from 'react';
 
 function App() {
+
   return (
-    <div className="App">
+    <div className="mainpage">
+      <div className="App">
         <h1>Notes Application</h1>
         <p>Enter your notes below</p>
         <div className='formHolder'>
-          <div className="mb-3">
+          <form action='post' className="mb-3">
             <label htmlFor="note" className='form-label'>Notes :</label>
-            <input type='text' className='form-control' id='note' name='note' placeholder='Enter your notes'/>
-            <br />
-            <button type='button' className='notesButton' id='myButton'>Add note</button>
-          </div>
+            <input type='text' className='form-control' id='note' name='note' placeholder='Enter your title' />
+            <textarea className='form-control' id='noteDesc' name='note' rows={3} placeholder='Description' />
+            <button type='submit' className='notesButton' id='myButton'>Add note</button>
+          </form>
         </div>
+      </div>
     </div>
   );
 }
