6965dbc6d28a70c05e88acc56d32f267df3b4432
AjneoDalipi <ajneodalipi04@gmail.com>
2023-01-27 02:28:20 +0100

    Working with DB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res)=> {
-    res.send("");
-})
+const PORT = process.env.PORT || 4000;
 
-app.listen(3001, () => {
-    console.log("port 3001 running")
+app.set('view engine', "ejs");
+
+app.get('/', (req, res)=>{
+    res.render('app');
+});
+
+app.get('/users/register', (req, res)=>{
+    res.render('register');
+});
+
+app.get('/users/login', (req, res)=>{
+    res.render('login');
+});
+
+app.get('/users/dashboard', (req, res)=>{
+    res.render("dashboard", { user: "Mate" });
+});
+
+app.listen(PORT, ()=>{
+    console.log(PORT);
 })
